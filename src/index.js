f90751941da139094c30e07dc0585081c0c92e72
Div-yanshi <2k22.csds.ng2.2212527@gmail.com>
2025-08-08 00:01:21 +0530

    Added server and routes files

--- src/index.js
+++ src/index.js
@@ -11,13 +11,17 @@ dotenv.config({
 
 console.log("MONGODB_URI:", process.env.MONGODB_URI);
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port:${process.env.PORT}`);
+    })
 
+})
 
-
-
-
-
+.catch((err) => {
+    console.log("MONGO db connection failed!!!", err);
+})
 
 
 
